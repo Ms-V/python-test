eb322b5e6aac9a8213dc9faa2ef0947198529f6b
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2020-08-06 15:01:34 +0800

    Add files via upload

---
+++ 1-3.py
@@ -0,0 +1,40 @@
+js="哭!都他妈给我哭!"
+print(js)
+js1="可劲儿哭!"
+print(js1)
+js2="'牛逼'"
+print(js2)
+Big_test='nt xIaochUi'
+js_1_2=js+js1
+print(Big_test.title())
+print(Big_test.upper())
+print(Big_test.lower())
+print(js_1_2)
+print(js+js1+js2)
+print("绝世大李琦:"+js_1_2)
+print(Big_test.upper()+"屁王")
+js_f=Big_test.upper()+"绝世大李琦:"+"脑瘫！"
+print(js_f)
+print("怎么说\t就是这么叼\n哈哈哈\n哈哈哈")
+kongge="     P   Padada    "
+print(kongge+"tt")
+print(kongge.rstrip()+"tt")
+print(kongge.lstrip()+"tt")
+print(kongge.strip()+"tt")
+print(3*3)
+print(3+3)
+print(3**3)
+print((2+1)*3)
+print(3-3)
+print(3/3)
+print(0.3+0.3)
+print(0.3-0.3)
+print(0.3*0.3)
+age=19
+print("Now"+str(age)+"????")
+print(3/2)
+ast=3
+print("bb is:",ast+ast)
+print(int(ast)*int(ast))
+print(ast+ast)
+#yooooooooooooo.........
