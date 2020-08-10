5f49b8e6f286eff300d391396f339b04d38b2801
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2020-08-10 14:39:44 +0800

    Add files via upload

---
+++ 1-5.py
@@ -0,0 +1,54 @@
+ri=['fa','fdd','hjl','qda']
+for rii in ri:
+    print(rii.upper()+"????")
+    print("yuri")
+print(rii)
+print('all done')
+for bt in range(1,10):
+    print(bt)
+bt_list=list(range(1,4))
+print(bt_list)
+robt=list(range(3,25,3))
+print(robt)
+setp=[]
+for va in range(1,11):
+    setp.append(va**3)
+print(setp)
+dig=list(range(1,20,2))
+print(dig)
+print(min(dig))
+print(max(dig))
+print(sum(dig))
+sq=[va**2 for va in range(1,11)]
+print(sq)
+in20=list(range(1,21))
+print(in20)
+hgn=['li qi','jue shi','fei fei','wang']
+print(hgn[0:3])
+print(in20[0:10])
+print(hgn[1:4])
+print(in20[:7])
+print(in20[3:])
+print(in20[-6:])
+print(in20[-6:-3])
+print('list is :')
+for l_in20 in in20[:10]:
+    print(str(l_in20)+"\n")
+in10=in20[:10]
+in20_2=in20[:]
+print(in10)
+in20_2.append(21)
+print(in20_2)
+fd=['naotan','xiaochui','xiaoxiaochui']
+fd_2=fd
+print(fd,fd_2)
+fd.append('liqi')
+fd_2.append('jueshi')
+print(fd,fd_2)
+dim=('aka','bkb','liqi')
+print(dim)
+print(dim[1])
+for va in dim:
+    print(va)
+dim=('liqi','daruozhi','caia')
+print(dim)
