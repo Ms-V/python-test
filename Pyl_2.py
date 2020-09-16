d2beb6f37bf4b250d06784f060ae4bb5a0bdaccb
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2020-09-16 08:40:46 +0800

    Add files via upload

---
+++ Pyl_2.py
@@ -0,0 +1,9 @@
+import os
+import sys
+print('gbg'*5)
+pathpy=os.getcwd()#查看当前系统python路径，今天python的path出毛病了，用这个看了下重新设置
+#了下path没用，最后还是重新装了遍就好了
+print(pathpy)
+print(sys.platform)#查看系统平台
+for x in 'naizi!':
+	print(x)
